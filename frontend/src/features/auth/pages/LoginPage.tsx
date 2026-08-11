ffecbd46ba79e926dc271333de70f2bc4da43d16
Eze Belmonte <eze.m.belmonte@gmail.com>
2026-08-11 20:15:16 -0300

    Arreglado el logo en el login

--- frontend/src/features/auth/pages/LoginPage.tsx
+++ frontend/src/features/auth/pages/LoginPage.tsx
@@ -21,11 +21,12 @@ const LoginPage = () => {
         sm:grid-cols-2
         sm:items-center
       ">
-        <Logo 
-          className="w-[200px] sm:w-[250px] md:w-[300px]"
-          centered
-          textX={50}
-        />
+  
+          <Logo 
+            className="w-full flex items-center justify-center"
+            iconClassName="w-[90px] md:w-[120px] lg:w-[170px]"
+            textClassName="text-white text-[3rem] md:text-[3.2rem] lg:text-[3.7rem]"
+          />
 
         <div className="
           flex flex-col
